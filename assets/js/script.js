c95c04d31d1708789fb16b3f5f338f69a265ee6a
Chice <cko517@hotmail.com>
2021-10-31 23:48:16 -0400

    divs and currentday attempt

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// set up current date with Day, Month, Date format
+var currentDay = document.querySelector("#currentDay");
+
+currentDay.moment()
+
+
+// scheduler container
+
+// give each timeblock a time id and class based on time and screen it
+// append to div id currentDay (innerText)
+
+// div for element displaying time, div for middle text box, div for save button
+
+// color code time blocks based on past, present, future
+// set classes through js to change colors based on current time
+
+// click listener on button save text box/prompt event to localStorage
+// key: value
+// key - time
+// value - text
+
+// check save
+// if no save, create empty object/array/etc.
+// otherwise fill timeblocks with localStorage
